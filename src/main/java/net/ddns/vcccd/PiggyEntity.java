a7653b0f09490bcc015a7841def84e5fe9ca92e5
ApoloG6 <williamdrosado@yahoo.com>
2025-11-26 00:20:24 -0600

    Refactor PiggyEntity to use PiglinBrute
    Updated PiggyEntity to use PiglinBrute instead of deprecated PigZombie. Enhanced armor and drop chances for the entity.

--- src/main/java/net/ddns/vcccd/PiggyEntity.java
+++ src/main/java/net/ddns/vcccd/PiggyEntity.java
@@ -7,39 +7,57 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.EntityType;
-import org.bukkit.entity.PigZombie;
+import org.bukkit.entity.PiglinBrute;
+import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class PiggyEntity {
-	
-	private ItemStack PiggySword = new ItemStack(Material.GOLDEN_AXE);
-	private List<String> PiggyLore = new ArrayList<String>();
-	
-	private ItemStack[] apperal = {
-			new ItemStack(Material.DIAMOND_BOOTS),
-            new ItemStack(Material.DIAMOND_LEGGINGS),
-            new ItemStack(Material.DIAMOND_CHESTPLATE)
-	};
-	
-	@SuppressWarnings("deprecation")
-	public PiggyEntity(int health, Location location, World world, Main main) {
-		PigZombie OurPiggy = (PigZombie) world.spawnEntity(location, EntityType.ZOMBIFIED_PIGLIN);
-		OurPiggy.setMaxHealth(health);
-		OurPiggy.setHealth(health);
-		OurPiggy.getEquipment().setArmorContents(apperal);
-		ItemMeta PiggySwordMeta = PiggySword.getItemMeta();
-		PiggySwordMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&c&lPiggy&4&lAxe"));
-		this.PiggyLore.add(ChatColor.translateAlternateColorCodes('&', "&cThe power of the piggy"));
-		PiggySwordMeta.setLore(PiggyLore);
-		this.PiggySword.setItemMeta(PiggySwordMeta);
-		OurPiggy.getEquipment().setItemInMainHand(PiggySword);
-		OurPiggy.setCustomName(ChatColor.translateAlternateColorCodes('&', "&c&lPiGgY"));
-		OurPiggy.setCustomNameVisible(true);
-		OurPiggy.setAdult();
-		OurPiggy.setRemoveWhenFarAway(main.getConfig().getBoolean("PiggyDeSpawn"));
-		
-	}
 
+    private ItemStack PiggySword = new ItemStack(Material.GOLDEN_AXE);
+    private List<String> PiggyLore = new ArrayList<String>();
+
+    private ItemStack[] apperal = {
+            new ItemStack(Material.NETHERITE_BOOTS),
+            new ItemStack(Material.NETHERITE_LEGGINGS),
+            new ItemStack(Material.NETHERITE_CHESTPLATE),
+            new ItemStack(Material.NETHERITE_HELMET)
+    };
+
+    public PiggyEntity(int health, Location location, World world, Main main) {
+        // Use PiglinBrute instead of deprecated PigZombie
+        PiglinBrute OurPiggy = (PiglinBrute) world.spawnEntity(location, EntityType.PIGLIN_BRUTE);
+
+        OurPiggy.getAttribute(Attribute.MAX_HEALTH).setBaseValue(health);
+        OurPiggy.setHealth(health);
+
+        ItemMeta PiggySwordMeta = PiggySword.getItemMeta();
+        PiggySwordMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', "&c&lPiggy&4&lAxe"));
+        this.PiggyLore.add(ChatColor.translateAlternateColorCodes('&', "&cThe power of the piggy"));
+        PiggySwordMeta.setLore(PiggyLore);
+        this.PiggySword.setItemMeta(PiggySwordMeta);
+
+        EntityEquipment equipment = OurPiggy.getEquipment();
+        if (equipment != null) {
+            equipment.setBoots(apperal[0]);
+            equipment.setLeggings(apperal[1]);
+            equipment.setChestplate(apperal[2]);
+            equipment.setHelmet(apperal[3]);
+
+            equipment.setItemInMainHand(PiggySword);
+
+            equipment.setItemInMainHandDropChance(1.0f); // 100% chance to drop the axe
+            equipment.setHelmetDropChance(0.0f); // Don't drop armor
+            equipment.setChestplateDropChance(0.0f);
+            equipment.setLeggingsDropChance(0.0f);
+            equipment.setBootsDropChance(0.0f);
+        }
+
+        OurPiggy.setCustomName(ChatColor.translateAlternateColorCodes('&', "&c&lPiGgY"));
+        OurPiggy.setCustomNameVisible(true);
+
+        OurPiggy.setRemoveWhenFarAway(main.getConfig().getBoolean("PiggyDeSpawn"));
+    }
 }
